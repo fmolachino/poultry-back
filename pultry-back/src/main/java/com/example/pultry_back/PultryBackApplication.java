d50b1ddcb2b8afd112ffd2ea99887a12bfb73ef4
fmolachino <fmolachino@gmail.com>
2024-12-25 15:45:55 -0300

    added springboot project

---
+++ pultry-back/src/main/java/com/example/pultry_back/PultryBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.pultry_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PultryBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PultryBackApplication.class, args);
+	}
+
+}
