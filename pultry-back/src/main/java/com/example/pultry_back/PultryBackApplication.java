4076b23a74887edaff7698df17c2a188e060da94
fmolachino <fmolachino@gmail.com>
2025-01-06 16:32:36 -0300

    last point using jpa, migrating to jdbc

--- pultry-back/src/main/java/com/example/pultry_back/PultryBackApplication.java
+++ pultry-back/src/main/java/com/example/pultry_back/PultryBackApplication.java
@@ -1,14 +1,14 @@
 package com.example.pultry_back;
 
-import java.util.NoSuchElementException;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import com.example.pultry_back.entity.User;
-import com.example.pultry_back.repository.UserRepository;
+// import java.util.NoSuchElementException;
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.context.annotation.Bean;
+// import com.example.pultry_back.entity.User;
+// import com.example.pultry_back.repository.UserRepository;
 
 @SpringBootApplication
 public class PultryBackApplication {
